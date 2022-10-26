38d1603be2738318dcde3b580fa202d42c4fc530
reed426 <red426@sookmyung.ac.kr>
2022-10-26 23:39:22 +0900

    회원 서비스 테스트

--- src/main/java/hello/springintroduction/service/MemberService.java
+++ src/main/java/hello/springintroduction/service/MemberService.java
@@ -1,2 +1,45 @@
-package hello.springintroduction.service;public class MemberService {
+package hello.springintroduction.service;
+
+import hello.springintroduction.domain.Member;
+import hello.springintroduction.repository.MemberRepository;
+import hello.springintroduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원은 받지않음.
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+           throw new IllegalStateException("이미 존재하는 회원입니다.");
+       });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
